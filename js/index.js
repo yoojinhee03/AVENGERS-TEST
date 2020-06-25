b32a67af0037e05e1ab8263843109c7c086a1f74
yoojinhee03 <yoojinhee03@gmail.com>
2020-06-25 23:30:15 +0900

    이미지 추가

--- js/index.js
+++ js/index.js
@@ -4,8 +4,22 @@
     // the link to your model provided by Teachable Machine export panel
     const URL = "./my_model/";
     const arr = new Array();
-    let model, webcam, labelContainer, maxPredictions;
-
+    let model, webcam, labelContainer,imageContainer, maxPredictions;
+    const images= new Array(
+        "ironman.jpg",
+        "Hulk.jpg",
+        "BlackWidow.jpg",
+        "Captain.jpg",
+        "Hawkeye.jpg",
+        "Rocky.jpg",
+        "WinterSoldier.jpg",
+        "Falcon.jpg",
+        "StarRoad.jpg",
+        "Gamora.jpg",
+        "Scarlet.jpg",
+        "SpiderMan.jpg",
+        "Thor.jpg",
+    );
     // Load the image model and setup the webcam
     async function init() {
         const modelURL = URL + "model.json";
@@ -20,7 +34,7 @@
 
         // Convenience function to setup a webcam
         const flip = true; // whether to flip the webcam
-        webcam = new tmImage.Webcam(200, 200, flip); // width, height, flip
+        webcam = new tmImage.Webcam(400, 400, flip); // width, height, flip
         await webcam.setup(); // request access to the webcam
         await webcam.play();
         window.requestAnimationFrame(loop);
@@ -28,14 +42,28 @@
         // append elements to the DOM
         document.getElementById("webcam-container").appendChild(webcam.canvas);
         labelContainer = document.getElementById("label-container");
-        for (let i = 0; i < maxPredictions; i++) { // and class labels
-            labelContainer.appendChild(document.createElement("div"));
-        }
+        labelContainer.appendChild(document.createElement("div"));
+        // for (let i = 0; i < maxPredictions; i++) { // and class labels
+        //     // labelContainer.appendChild(document.createElement("div"));
+        // }
+    }
+    async function stop(){
+        await webcam.pause(); // update the webcam frame
+        document.getElementById("label-container").style.display="block";
+        await predict();
+        changeButton("stop","replay");
+    }
+
+    async function replay(){
+        await webcam.play(); // request access to the webcam
+        document.getElementById("label-container").style.display="none";
+        window.requestAnimationFrame(loop);
+        changeButton("replay","stop");
     }
 
     async function loop() {
         webcam.update(); // update the webcam frame
-        await predict();
+        // await predict();
         window.requestAnimationFrame(loop);
     }
     // run the webcam image through the image model
@@ -48,13 +76,31 @@
         let max=prediction[0].probability.toFixed(2);
         let index=0;
         for (let i = 0; i < maxPredictions; i++){
-            if(prediction[i].probability.toFixed(2)>0.6){
-                if(max<prediction[i].probability.toFixed(2)){
-                    max=prediction[i].probability.toFixed(2);
-                    index=i;
-                }
+            if(max<prediction[i].probability.toFixed(2)){
+                max=prediction[i].probability.toFixed(2);
+                index=i;
             }
+            console.log(arr[i]+":"+prediction[i].probability.toFixed(2));
         }
         const classPrediction =arr[index] + ": " + max;
         labelContainer.childNodes[0].innerHTML = classPrediction;
+        imageContainer = document.getElementById("image-container");
+        imageContainer.style.display="inline-block";
+        imageContainer.appendChild(document.createElement("img"));
+        document.getElementById("wrapper").style.width="850px";
+        imageContainer.childNodes[0].src="./images/"+images[index];
+    }
+
+    function over(){
+        document.getElementById("over").style.display="none";
+    }
+    function changeButton(id,changeId){
+        document.getElementById(id).id=changeId;
+        if(changeId=="replay"){
+            document.getElementById(changeId).value="다시하기";
+            document.getElementById(changeId).setAttribute("onClick", "replay()");
+        }else{
+            document.getElementById(changeId).value="멈추기";
+            document.getElementById(changeId).setAttribute("onClick","stop()");
+        }
     }
